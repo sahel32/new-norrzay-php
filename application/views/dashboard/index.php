7439505849f02acb79c5eb443bda2cdd6363823a
sahel32 <sahel.keyhan@gmail.com>
2017-01-23 20:35:11 +0430

    dashboard changes

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -1,8 +1,8 @@
 <div id="page-inner">
     <div class="row">
         <div class="col-md-12">
-            <h2>Blank Page</h2>
-            <h5>Welcome Jhon Deo , Love to see you back. </h5>
+            <h2>مدیریت سیستم</h2>
+            <h5>از این قسمت میتوانید به تمام بخش های سیستم دسترسی داشته باشید.</h5>
 
         </div>
     </div>
@@ -10,60 +10,68 @@
     <hr />
 
     <div class="row">
-        <div class="col-md-3 col-sm-6 col-xs-12 ">
+        <div class="col-md-4 col-sm-8 col-xs-12 ">
             <div class="panel ">
                 <div class="main-temp-back stock">
                     <div class="panel-body">
                         <div class="row">
-                            <div class="col-xs-6"> <i><img src="assets/img/stock.png" alt="User"/></i></div>
-                            <div class="col-xs-6">
-                                <div class="text-temp"> گدام </div>
-                            </div>
+                            <a href="<?php  echo site_url('stock/lists');?>" class="fix-color">
+                                <div class="col-xs-6"> <i><img src="<?php echo asset_url('../assets/img/stock.png'); ?>" alt="User"/></i></div>
+                                <div class="col-xs-6">
+                                    <div class="text-temp">گدام ها</div>
+                                </div>
+                            </a>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
 
-        <div class="col-md-3 col-sm-6 col-xs-12 ">
+        <!-- <div class="col-md-3 col-sm-6 col-xs-12 "> 
             <div class="panel ">
                 <div class="main-temp-back safebox">
                     <div class="panel-body">
                         <div class="row">
-                            <div class="col-xs-6"> <i><img src="assets/img/safebox.png" alt="User"/></i></div>
-                            <div class="col-xs-6">
-                                <div class="text-temp"> صندوق </div>
-                            </div>
+                            <a href="<?php  echo site_url('stock/lists');?>" class="fix-color">
+                                <div class="col-xs-6"> <i><img src="<?php echo asset_url('../assets/img/safebox.png'); ?>" alt="User"/></i></div>
+                                <div class="col-xs-6">
+                                    <div class="text-temp"> صندوق </div>
+                                </div>
+                            </a>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </div> -->
 
-        <div class="col-md-3 col-sm-6 col-xs-12 ">
+        <div class="col-md-4 col-sm-8 col-xs-12 ">
             <div class="panel ">
                 <div class="main-temp-back buys">
                     <div class="panel-body">
                         <div class="row">
-                            <div class="col-xs-6"> <i><img src="assets/img/buys.png" alt="User"/></i></div>
-                            <div class="col-xs-6">
-                                <div class="text-temp"> خریدها </div>
-                            </div>
+                            <a href="<?php  echo site_url('oil/lists_pre_buy');?>" class="fix-color">
+                                <div class="col-xs-6"> <i><img src="<?php echo asset_url('../assets/img/buys.png'); ?>" alt="User"/></i></div>
+                                <div class="col-xs-6">
+                                    <div class="text-temp"> پیش خریدها </div>
+                                </div>
+                            </a>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
 
-        <div class="col-md-3 col-sm-6 col-xs-12 ">
+        <div class="col-md-4 col-sm-8 col-xs-12 ">
             <div class="panel ">
                 <div class="main-temp-back sells">
                     <div class="panel-body">
                         <div class="row">
-                            <div class="col-xs-6"> <i><img src="assets/img/sells.png" alt="User"/></i></div>
-                            <div class="col-xs-6">
-                                <div class="text-temp"> فروشات </div>
-                            </div>
+                            <a href="<?php  echo site_url('oil/lists_pre_sell');?>" class="fix-color">
+                                <div class="col-xs-6"> <i><img src="<?php echo asset_url('../assets/img/sells.png'); ?>" alt="User"/></i></div>
+                                <div class="col-xs-6">
+                                    <div class="text-temp">پیش فروش ها</div>
+                                </div>
+                            </a>
                         </div>
                     </div>
                 </div>
@@ -77,10 +85,12 @@
                 <div class="main-temp-back reports">
                     <div class="panel-body">
                         <div class="row">
-                            <div class="col-xs-6"> <i><img src="assets/img/reports.png" alt="User"/></i></div>
-                            <div class="col-xs-6">
-                                <div class="text-temp"> گزارشات </div>
-                            </div>
+                            <a href="<?php  echo site_url('balance/get_total_balance');?>" class="fix-color">
+                                <div class="col-xs-6"> <i><img src="<?php echo asset_url('../assets/img/reports.png'); ?>" alt="User"/></i></div>
+                                <div class="col-xs-6">
+                                    <div class="text-temp"> گزارشات </div>
+                                </div>
+                            </a>
                         </div>
                     </div>
                 </div>
@@ -92,7 +102,7 @@
                 <div class="main-temp-back backup">
                     <div class="panel-body">
                         <div class="row">
-                            <div class="col-xs-6"> <i><img src="assets/img/backup.png" alt="User"/></i></div>
+                            <div class="col-xs-6"> <i><img src="<?php echo asset_url('../assets/img/backup.png'); ?>" alt="User"/></i></div>
                             <div class="col-xs-6">
                                 <div class="text-temp"> پشتیبان گیری </div>
                             </div>
@@ -107,10 +117,12 @@
                 <div class="main-temp-back accounts">
                     <div class="panel-body">
                         <div class="row">
-                            <div class="col-xs-6"> <i><img src="assets/img/accounts.png" alt="User"/></i></div>
-                            <div class="col-xs-6">
-                                <div class="text-temp"> حساب ها </div>
-                            </div>
+                            <a href="<?php echo site_url("account/add") ?>" class="fix-color">
+                                <div class="col-xs-6"> <i><img src="<?php echo asset_url('../assets/img/accounts.png'); ?>" alt="User"/></i></div>
+                                <div class="col-xs-6">
+                                    <div class="text-temp"> افتتاح حساب </div>
+                                </div>
+                            </a>
                         </div>
                     </div>
                 </div>
@@ -122,9 +134,9 @@
                 <div class="main-temp-back debits">
                     <div class="panel-body">
                         <div class="row">
-                            <div class="col-xs-6"> <i><img src="assets/img/debits.png" alt="User"/></i></div>
+                            <div class="col-xs-6"> <i><img src="<?php echo asset_url('../assets/img/debits.png'); ?>" alt="User"/></i></div>
                             <div class="col-xs-6">
-                                <div class="text-temp"> پرداخت ها </div>
+                                <div class="text-temp"> دریافت / پرداخت </div>
                             </div>
                         </div>
                     </div>
@@ -137,7 +149,7 @@
                 <div class="main-temp-back credits">
                     <div class="panel-body">
                         <div class="row">
-                            <div class="col-xs-6"> <i><img src="assets/img/credits.png" alt="User"/></i></div>
+                            <div class="col-xs-6"> <i><img src="<?php echo asset_url('../assets/img/credits.png'); ?>" alt="User"/></i></div>
                             <div class="col-xs-6">
                                 <div class="text-temp"> دریافت ها </div>
                             </div>
